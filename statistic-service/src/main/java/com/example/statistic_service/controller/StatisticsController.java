5d473879b03879f754eedac6a67c4ad20e3a8889
Jessica Steinkamp <jessica@steinkamp.se>
2026-03-26 17:35:10 +0100

    added userstats with endpoint and service

--- statistic-service/src/main/java/com/example/statistic_service/controller/StatisticsController.java
+++ statistic-service/src/main/java/com/example/statistic_service/controller/StatisticsController.java
@@ -1,38 +1,52 @@
 package com.example.statistic_service.controller;
 
 import com.example.statistic_service.client.TrainingClient;
+import com.example.statistic_service.client.UserClient;
 import com.example.statistic_service.model.Statistics;
 import com.example.statistic_service.model.Training;
+import com.example.statistic_service.model.User;
 import com.example.statistic_service.service.StatisticsService;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
-@RequestMapping("/statistics")
+@RequestMapping("/statistic")
 public class StatisticsController {
 
-    private TrainingClient trainingClient;
-    private StatisticsService statisticsService;
+    private final TrainingClient trainingClient;
+    private final UserClient userClient;
+    private final StatisticsService statisticsService;
 
-    public StatisticsController(StatisticsService statisticsService, TrainingClient trainingClient) {
+    public StatisticsController(StatisticsService statisticsService, TrainingClient trainingClient, UserClient userClient) {
         this.statisticsService = statisticsService;
         this.trainingClient = trainingClient;
+        this.userClient = userClient;
     }
 
     @GetMapping("{id}")
     public List<Training> getTrainings(@PathVariable int id) {
         return trainingClient.getTrainings(id);
     }
+
     @GetMapping("/user/{id}")
-    public Statistics getUserTrainings(@PathVariable int id) {
-        List<Training> alltrainings = trainingClient.getTrainings(id);
-        return statisticsService.getStats(alltrainings);
+    public Statistics getTrainingsPerUser(@PathVariable int id) {
+        List<Training> allTrainings = trainingClient.getTrainings(id);
+        return statisticsService.getStats(allTrainings);
     }
+
     @GetMapping("/period/user/{id}")
-    public Statistics getUserTrainingsPerPeriod(@PathVariable int id, @RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
-        List<Training> alltrainings = trainingClient.getTrainings(id);
-        return statisticsService.getStatsPerPeriod(alltrainings, startDate, endDate);
+    public Statistics getTrainingsPerUserAndPeriod(@PathVariable int id, @RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
+        List<Training> allTrainings = trainingClient.getTrainings(id);
+        return statisticsService.getStatsPerPeriod(allTrainings, startDate, endDate);
+    }
+
+    @GetMapping("/users")
+    public HashMap<String, Integer> getUserStatistics (){
+        List<User> allUsers = userClient.getUsers();
+        List<Training> allTrainings = trainingClient.getAllTrainings();
+        return statisticsService.getUserStatistics(allTrainings, allUsers);
     }
 }
--- statistic-service/src/main/java/com/example/statistic_service/service/StatisticsService.java
+++ statistic-service/src/main/java/com/example/statistic_service/service/StatisticsService.java
@@ -2,35 +2,52 @@ package com.example.statistic_service.service;
 
 import com.example.statistic_service.model.Statistics;
 import com.example.statistic_service.model.Training;
+import com.example.statistic_service.model.User;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
 public class StatisticsService {
 
-    public Statistics getStats(List<Training> alltrainings) {
+    public Statistics getStats(List<Training> allTrainings) {
         int total = 0;
         int duration = 0;
-        for (Training training : alltrainings) {
+        for (Training training : allTrainings) {
             total ++;
             duration += training.getDuration();
         }
         double average = (double) duration / total;
-        return new Statistics(alltrainings.getFirst().getUserId(), total, duration, average);
+        return new Statistics(allTrainings.getFirst().getUserId(), total, duration, average);
     }
 
-    public Statistics getStatsPerPeriod(List<Training> alltrainings, LocalDate startDate, LocalDate endDate) {
+    public Statistics getStatsPerPeriod(List<Training> allTrainings, LocalDate startDate, LocalDate endDate) {
         int total = 0;
         int duration = 0;
-        for (Training training : alltrainings) {
+        for (Training training : allTrainings) {
             if(training.getDate().isBefore(endDate) && training.getDate().isAfter(startDate)) {
                 total ++;
                 duration += training.getDuration();
             }
         }
         double average = (double) duration / total;
-        return new Statistics(alltrainings.getFirst().getUserId(), total, duration, average);
+        return new Statistics(allTrainings.getFirst().getUserId(), total, duration, average);
+    }
+
+    public HashMap<String, Integer> getUserStatistics (List<Training> allTrainings, List<User> allUsers) {
+        HashMap<String, Integer> userStatistics = new HashMap<>();
+
+        for (User user : allUsers) {
+            int noOfTrainings = 0;
+            for (Training training : allTrainings) {
+                if(training.getUserId() == user.getId()){
+                    noOfTrainings++;
+                }
+            }
+            userStatistics.put(user.getName(), noOfTrainings);
+        }
+        return userStatistics;
     }
 }
